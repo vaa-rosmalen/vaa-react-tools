60408685bed6333b88ca3082e80340930e69f885
Justin Steenhuis <justinsteenhuis@gmail.com>
2020-04-17 10:35:49 +0200

    Fix build

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import { Default as Thing } from '../stories/Thing.stories';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { CardTable } from '../stories/Table.stories';
 
-// describe('Thing', () => {
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<Thing />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-// });
+describe('CardTable', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<CardTable />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
